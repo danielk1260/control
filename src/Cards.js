5424b49ec0e22afb238a25de6b469dbd8c33bf2c
Danielk1260 <Danielk1260@gmail.com>
2021-01-30 11:51:00 -0600

    Separacion de Objetos cards
    Se realizo cambio e importacion del objeto a otro archivo y carpeta

--- src/Cards.js
+++ src/Cards.js
@@ -1,26 +1,8 @@
 import React from 'react';
+import { items } from './data/cards'
 import { Card, Container, Divider, Header, Icon } from 'semantic-ui-react';
 
-const items = [
-  {
-    image: 'https://picsum.photos/300/300?grayscale&blur=1',
-    color: 'red', header: 'Imagen #1', meta: 'Color red',
-  },
-  {
-    image: 'https://picsum.photos/300/300?grayscale',
-    color: 'blue', header: 'Imagen #2', meta: 'Color blue',
-  },
-  {
-    image: 'https://picsum.photos/300/300?&blur',
-    color: 'teal', header: 'Imagen #3', meta: 'Color teal',
-  },
-  {
-    image: 'https://picsum.photos/300/300',
-    color: 'yellow', header: 'Imagen #4', meta: 'Color yellow',
-  },
-];
-
-const Cards = () => ( 
+const Cards = () => (
   <Container>
     
     <Divider horizontal>
---
+++ src/data/cards.js
@@ -0,0 +1,18 @@
+export const items = [
+  {
+    image: 'https://picsum.photos/300/300?grayscale&blur=1',
+    color: 'red', header: 'Imagen #1', meta: 'Color red',
+  },
+  {
+    image: 'https://picsum.photos/300/300?grayscale',
+    color: 'blue', header: 'Imagen #2', meta: 'Color blue',
+  },
+  {
+    image: 'https://picsum.photos/300/300?&blur',
+    color: 'teal', header: 'Imagen #3', meta: 'Color teal',
+  },
+  {
+    image: 'https://picsum.photos/300/300',
+    color: 'yellow', header: 'Imagen #4', meta: 'Color yellow',
+  },
+];
